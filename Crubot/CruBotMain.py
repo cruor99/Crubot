cb3b868dc00ddc5a1c725e689b0eb8161632916f
KjetilAndre <kliknes@gmail.com>
2013-12-26 20:34:49 +0100

    Started usability work

--- Crubot/CruBotMain.py
+++ Crubot/CruBotMain.py
@@ -1,13 +1,20 @@
+
 import socket
 import sys
 import json
 import requests
+import os
 
 server = "irc.twitch.tv"       #settings
-channel = "#cruor99"
+channel = raw_input("What is your Twitch name")
+
 botnick = "CruBot"
 password = "oauth:ca3muz1anz4eddy93v1f2a41lyotiqg"
-
+auth_file = open('accept_token/'+channel+'.txt', 'w')
+auth_file = open('accept_token/'+channel+'.txt', 'r')
+botAuth = str(auth_file.readlines())
+print botAuth
+auth_file.close()
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
 print "connecting to:"+server
 irc.connect((server, 6667))
@@ -15,7 +22,7 @@ irc.send("PASS "+ password +"\n")                                               
 irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is a fun bot!\n") #user authentication
 irc.send("NICK "+ botnick +"\n")                            #sets nick
 irc.send("PRIVMSG nickserv :iNOOPE\r\n")    #auth
-irc.send("JOIN "+ channel +"\n")        #join the chan
+irc.send("JOIN #"+ channel +"\n")        #join the chan
 
 def chanUpdate (*title): #updates channel title
   
@@ -23,14 +30,19 @@ def chanUpdate (*title): #updates channel title
     print example
     print title
     
-    url = 'https://api.twitch.tv/kraken/channels/cruor99?oauth_token=niyi9fepwmus1pge3rosn2lqn4wy46n'
-    headers = {"Accept ": "application/vnd.twitchtv.v2+json", " Authorization": " OAuth niyi9fepwmus1pge3rosn2lqn4wy46n"}
-    channel = {'channel[status]': title}
-    r = requests.put(url, data=channel, headers=headers)
-    #test = requests.get(headers=headers, url=url)
-    #print test.text
+    url = 'https://api.twitch.tv/kraken/channels/'+channel+'?oauth_token=niyi9fepwmus1pge3rosn2lqn4wy46n'
+    headers = {"Accept ": "application/vnd.twitchtv.v2+json", " Authorization": " "+botAuth}
+    channelobj = {'channel[status]': title}
+    r = requests.put(url, data=channelobj, headers=headers)
     print r.text
 
+def storeAuth(accepttoken): #Saves the accept token in a file
+	workPath = os.getcwd()
+	os.chdir(workPath +"/accept_token")
+	auth_token = open(channel + ".txt", "w")
+	auth_token.write(accepttoken)
+	
+	
 while 1:    #puts it in a loop
     text=irc.recv(2040)  #receive the text
     print text   #print text to console
@@ -52,10 +64,13 @@ while 1:    #puts it in a loop
         strTest = "Vidya: ".join(t[1:])
         print strTest
         chanUpdate(strTest)
-        
+    if text.find(':!setToken') !=-1:
+		t = text.split(':!setToken') 
+		storeAuth(t[1])
 
 
 #https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=33u2pg1xqnjj6okkg2ld4o9wclmztb1&redirect_uri=http://thatothervidyastream.webs.com/auth.html&scope=channel_editor+channel_read+user_read
 
 
 #niyi9fepwmus1pge3rosn2lqn4wy46n
+#vidya stream auth: 9jhojix8jia8lfon1agqxl914swtr72
